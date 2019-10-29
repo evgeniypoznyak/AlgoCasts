896ac4ee66a27f46c189a2cb56c7add2f2bfe1b7
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2019-10-28 23:57:58 -0500

    maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const result = {};
+    for (const i of str){
+        result[i] = result[i] + 1 || 1;
+        // if (result[i]  !== undefined) {
+        //     result[i]++;
+        // } else {
+        //     result[i] = 1;
+        // }
+    }
+
+    let max = Number.MIN_SAFE_INTEGER;
+    let index;
+    for (const i in result) {
+        if (result[i] > max) {
+            max = result[i];
+            index = i;
+        }
+    }
+    return index;
+}
 
 module.exports = maxChar;
