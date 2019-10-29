71df11f6e3128680af8dda8875e290fd278bf008
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2019-10-29 17:08:47 -0500

    chunk another approach

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -9,20 +9,36 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-    const result = [];
-    let tempArray = [];
-    for (let i = 0; i < array.length; i++) {
-        if (i !== 0 && i % size === 0) {
-            result.push(tempArray);
-            tempArray = [];
-        }
-        tempArray.push(array[i]);
-        if (i+1 === array.length && tempArray.length !== 0) {
-            result.push(tempArray);
-        }
-    }
-    return result;
+    // const result = [];
+    // let tempArray = [];
+    // if (array.length < size) {
+    //     return array;
+    // }
+    // for (let i = 0; i < array.length; i++) {
+    //     if (i !== 0 && i % size === 0) {
+    //         result.push(tempArray);
+    //         tempArray = [];
+    //     }
+    //     tempArray.push(array[i]);
+    //     if (i+1 === array.length && tempArray.length !== 0) {
+    //         result.push(tempArray);
+    //     }
+    // }
+    // return result;
 
+    const chunked = [];
+    for (let element of array){
+        // get the pointer for element in array to modify it
+        const last = chunked[chunked.length - 1];
+        if (!last || last.length === size) {
+            chunked.push([element]);
+        }else{
+            // modify chunked by modifying pointer
+            last.push(element);
+        }
+
+    }
+    return chunked;
 }
 
 module.exports = chunk;
