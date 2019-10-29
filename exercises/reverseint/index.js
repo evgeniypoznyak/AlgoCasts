4d0ace45ec27c85f450dfbafd4778e6e07e76544
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2019-10-28 19:13:18 -0500

    reverseInt + jest config

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,15 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    if (n === 0) {
+        return 0;
+    }
+    if (Math.sign(n)  === 1) {
+        return parseInt(n.toString().split('').reverse().join(''))
+    }else {
+        return -parseInt(n.toString().split('').reverse().join(''))
+    }
+}
 
 module.exports = reverseInt;
