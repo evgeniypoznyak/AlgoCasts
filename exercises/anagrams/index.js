f4f85468761248cffc1e213639447db69e65a3af
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2019-11-05 08:37:09 -0600

    solution 2

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,40 +8,44 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-const stringToObjectMap = string => {
-    const result = {};
-    for (let i in string) {
-        result[string[i]] = result[string[i]] + 1 || 1;
-        // if (!result[string[i]]) {
-        //     result[string[i]] = 1;
-        // } else {
-        //     result[string[i]]++;
-        // }
-    }
-    return result;
-};
+// const stringToObjectMap = string => {
+//     const result = {};
+//     for (let i in string) {
+//         result[string[i]] = result[string[i]] + 1 || 1;
+//         // if (!result[string[i]]) {
+//         //     result[string[i]] = 1;
+//         // } else {
+//         //     result[string[i]]++;
+//         // }
+//     }
+//     return result;
+// };
+
+const cleanAndSortString = str => str.replace(/[^\w]/g, '').toLocaleLowerCase().split('').sort().join('');
 
 function anagrams(stringA, stringB) {
-    stringA = stringA.replace(/[^\w]/g, '').toLocaleLowerCase();
-    stringB = stringB.replace(/[^\w]/g, '').toLocaleLowerCase();
-    if (stringA.length !== stringB.length) {
-        return false;
-    }
-    let objectA = {};
-    let objectB = {};
+    return cleanAndSortString(stringA) === cleanAndSortString(stringB);
 
-    objectA = stringToObjectMap(stringA);
-    objectB = stringToObjectMap(stringB);
-
-    for (const char in objectA) {
-        if (!objectB[char]) {
-            return false;
-        }
-        if (objectA[char] !== objectB[char]) {
-            return false;
-        }
-    }
-    return true;
+    // stringA = stringA.replace(/[^\w]/g, '').toLocaleLowerCase();
+    // stringB = stringB.replace(/[^\w]/g, '').toLocaleLowerCase();
+    // if (stringA.length !== stringB.length) {
+    //     return false;
+    // }
+    // let objectA = {};
+    // let objectB = {};
+    //
+    // objectA = stringToObjectMap(stringA);
+    // objectB = stringToObjectMap(stringB);
+    //
+    // for (const char in objectA) {
+    //     if (!objectB[char]) {
+    //         return false;
+    //     }
+    //     if (objectA[char] !== objectB[char]) {
+    //         return false;
+    //     }
+    // }
+    // return true;
 }
 
 module.exports = anagrams;
