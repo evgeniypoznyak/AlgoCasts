ff7a8585e3fc2ca94c10e894c24e9796a80bb558
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2019-11-05 17:01:21 -0600

    capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,9 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    return str.split(' ').map(char => char[0].toUpperCase() + char.slice(1)).join(' ');
+    // return str.split(' ').map(char => char.charAt(0).toUpperCase() + char.slice(1)).join(' ');
+}
 
 module.exports = capitalize;
